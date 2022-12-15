cb1f718ade5ce95af7a2b6e947691657935540ed
Giwrgos Semaniakou <geo.sem@hotmail.com>
2022-12-15 16:11:24 +0200

    First Commit

---
+++ src/emailApp/EmailApp.java
@@ -0,0 +1,16 @@
+package emailApp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        //Connect constructor and make it work from Email(class) to EmailApp(class)
+        Email em1= new Email("George","Semaniakou");
+        System.out.println(em1.showInfo());
+
+    }
+
+
+
+
+
+}
